bed6bb9509c3c9ad2fc3f207defbdd1a5af02ea5
Sohail Siadat <sohale@gmail.com>
2015-05-23 16:22:11 +0100

    Now the web-sockets work for Bcw; for transferring the change of users choices (previous example usage disabled).

--- src/main/java/sboot/websocket/MyMessageJson.java
+++ src/main/java/sboot/websocket/MyMessageJson.java
@@ -4,7 +4,7 @@ package sboot.websocket;
  * Created by sohail on 23/05/15.
  */
 public class MyMessageJson { //was: Greeting .content
-    private String txt;
+    private String txt; //private field name is not cared for in this class only.
 
     public MyMessageJson(String text) {
         this.txt=text;
